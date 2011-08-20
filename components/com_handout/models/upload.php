9b105ed0ddf7bccf7b8940db13e0bc2d318e04c9
Md Habibur Rahman <mhhabibict@gmail.com>
2011-08-21 03:06:56 +0600

    almost model of front end done only without upload model. these was created by modification of helpers file. we can remove most of helper file finally.

---
+++ components/com_handout/models/upload.php
@@ -0,0 +1,35 @@
+<?php 
+
+/**
+ * Handout - The Joomla Download Manager
+ * @package 	Handout
+ * @copyright 	(C) 2011 Kontent Design. All rights reserved.
+ * @copyright 	(C) 2003-2008 The DOCman Development Team
+ * @copyright 	(C) 2009 Artio s.r.o.
+ * @license 	http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link 		http://www.sharehandouts.com
+ */
+
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.model');
+
+class HandoutModelUpload extends JModel
+{
+	function __construct()
+	{
+		
+		parent::__construct();
+	}
+	
+	
+	
+	
+}
+
+
+
+
+
+
+?>
